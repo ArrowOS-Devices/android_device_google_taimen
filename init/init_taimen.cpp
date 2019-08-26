fd6acb6962f11cd74e43a92f4ad2e3226d41f948
AbhiShek Aggarwal <warabhishek@gmail.com>
2019-08-26 17:40:14 -0400

    taimen: Add custom libinit to set correct variants
    Change-Id: I04f09a14cf41c89b3135c53e3089d5c3e703aae0

---
+++ init/init_taimen.cpp
@@ -0,0 +1,71 @@
+/*
+   Copyright (c) 2015, The Linux Foundation. All rights reserved.
+   Copyright (C) 2016 The CyanogenMod Project.
+   Copyright (C) 2019-2020 The LineageOS Project.
+   Redistribution and use in source and binary forms, with or without
+   modification, are permitted provided that the following conditions are
+   met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above
+      copyright notice, this list of conditions and the following
+      disclaimer in the documentation and/or other materials provided
+      with the distribution.
+    * Neither the name of The Linux Foundation nor the names of its
+      contributors may be used to endorse or promote products derived
+      from this software without specific prior written permission.
+   THIS SOFTWARE IS PROVIDED "AS IS" AND ANY EXPRESS OR IMPLIED
+   WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+   MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON-INFRINGEMENT
+   ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS
+   BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+   CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+   SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+   BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY,
+   WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE
+   OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
+   IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <fstream>
+#include <unistd.h>
+#include <vector>
+
+#include <android-base/properties.h>
+#define _REALLY_INCLUDE_SYS__SYSTEM_PROPERTIES_H_
+#include <sys/_system_properties.h>
+
+#include "property_service.h"
+#include "vendor_init.h"
+
+using android::base::GetProperty;
+
+void property_override(char const prop[], char const value[], bool add = true) {
+    prop_info *pi;
+
+    pi = (prop_info *)__system_property_find(prop);
+    if (pi) {
+        __system_property_update(pi, value, strlen(value));
+    } else if (add) {
+        __system_property_add(prop, strlen(prop), value, strlen(value));
+    }
+}
+
+void full_property_override(const std::string &prop, const char value[]) {
+    const int prop_count = 6;
+    const std::vector<std::string> prop_types
+        {"", "odm.", "product.", "system.", "system_ext.", "vendor."};
+
+    for (int i = 0; i < prop_count; i++) {
+        std::string prop_name = "ro." + prop_types[i] + prop;
+        property_override(prop_name.c_str(), value);
+    }
+}
+
+void vendor_load_properties() {
+    const char *fingerprint = "google/taimen/taimen:11/RP1A.201005.004.A1/6934943:user/release-keys";
+    const char *description = "taimen-user 11 RP1A.201005.004.A1 6934943 release-keys";
+
+    full_property_override("build.fingerprint", fingerprint);
+    full_property_override("build.description", description);
+}
