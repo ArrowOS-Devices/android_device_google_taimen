1ef529bafec111d01c64b0ed4d547c1a79714632
ReallySnow <reallysnow233@gmail.com>
2020-09-17 21:13:32 -0400

    taimen: Initial Arrow bringup
    Change-Id: I9b152cc8fc8179a44dc255c202814de69dcdd2f9

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -17,6 +17,10 @@
 PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/aosp_taimen.mk \
     $(LOCAL_DIR)/aosp_taimen_hwasan.mk \
+    $(LOCAL_DIR)/arrow_taimen.mk
 
 COMMON_LUNCH_CHOICES := \
-    aosp_taimen-userdebug
+    aosp_taimen-userdebug \
+    arrow_taimen-eng \
+    arrow_taimen-user \
+    arrow_taimen-userdebug
---
+++ arrow_taimen.mk
@@ -0,0 +1,43 @@
+# Copyright (C) 2017-2020 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit some common Project arrow stuff.
+$(call inherit-product, vendor/arrow/config/common.mk)
+
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+
+# Inherit AOSP device configuration for Taimen
+$(call inherit-product, device/google/taimen/aosp_taimen.mk)
+
+# Override AOSP build properties
+PRODUCT_NAME := arrow_taimen
+PRODUCT_DEVICE := taimen
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Pixel 2 XL
+PRODUCT_RESTRICT_VENDOR_FILES := false
+
+DEVICE_MAINTAINER := ReallySnow
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRODUCT_NAME="taimen" \
+    PRIVATE_BUILD_DESC="taimen-user 11 RP1A.201005.004.A1 6934943 release-keys"
+
+BUILD_FINGERPRINT := "google/taimen/taimen:11/RP1A.201005.004.A1/6934943:user/release-keys"
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.build.fingerprint=google/taimen/taimen:11/RP1A.201005.004.A1/6934943:user/release-keys
+
+$(call inherit-product, vendor/google/taimen/taimen-vendor.mk)
